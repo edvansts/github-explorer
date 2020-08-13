a7fe720f4e75ea57216f9773b61e37fb48f27e4f
Edvan Santos <edvan.santos069@academico.ifs.edu.br>
2020-08-13 00:13:45 -0300

    Style Additions

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { FiChevronRight} from 'react-icons/fi'
+
+import { Title, Form, Repositories } from "./styles";
+import logoImg from "./../../assets/logo.svg";
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+      <Form>
+        <input placeholder='Digite o nome do repositório...'></input>
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href='teste'>
+          <img src="https://avatars0.githubusercontent.com/u/53586642?s=460&u=66ac9f0aeeae4ce207056643273308559abd79d7&v=4" 
+          alt="Edvan Santos"/>
+          <div>
+            <strong>typescript_initial</strong>
+            <p>Repository with learnings for Typescript</p>
+          </div>
+          <FiChevronRight size={24} />
+        </a>
+        <a href='teste'>
+          <img src="https://avatars0.githubusercontent.com/u/53586642?s=460&u=66ac9f0aeeae4ce207056643273308559abd79d7&v=4" 
+          alt="Edvan Santos"/>
+          <div>
+            <strong>typescript_initial</strong>
+            <p>Repository with learnings for Typescript</p>
+          </div>
+          <FiChevronRight size={24} />
+        </a>
+        <a href='teste'>
+          <img src="https://avatars0.githubusercontent.com/u/53586642?s=460&u=66ac9f0aeeae4ce207056643273308559abd79d7&v=4" 
+          alt="Edvan Santos"/>
+          <div>
+            <strong>typescript_initial</strong>
+            <p>Repository with learnings for Typescript</p>
+          </div>
+          <FiChevronRight size={24} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
